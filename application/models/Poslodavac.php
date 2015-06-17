36b281fed368c50f9a571885754b71faca4bb340
Sara <sara.puljic92@gmail.com>
2015-06-17 13:09:45 +0200

    commit

--- application/models/Poslodavac.php
+++ application/models/Poslodavac.php
@@ -2,17 +2,12 @@
 
 class Poslodavac extends Core\Model{
 	
-public function selectFromPoslodavac(){
-	$this->_table="poslodavac";
-	$registracija = $this->selectAll();
-	return $registracija;
-	var_dump($registracija);
-}
 
-	
 public function insert($ime_kontakt_osobe,$prezime_kontakt_osobe,$naziv_tvrtke){
   	$registracija = mysql_query("INSERT poslodavac(ime_kontakt_osobe,prezime_kontakt_osobe,naziv_tvrtke) VALUES('$ime_kontakt_osobe','$prezime_kontakt_osobe','$naziv_tvrtke')");
   	return $registracija;
+  	
+  	var_dump($registracija);
  	}
 
 
---
+++ application/views/poslodavac/registracija.php
@@ -0,0 +1,30 @@
+
+<?php echo $registracija; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+</head>
+
+<body>
+
+	<form method="post">
+	
+	<input type="text" name="ime_kontakt_osobe"/>
+	
+	<input type="text" name="prezime_kontakt_osobe"/>
+	
+	<input type="text" name="naziv_tvrtke"/>
+	
+	
+	
+	
+	
+	</form>
+
+
+</body>
+
+</html>
