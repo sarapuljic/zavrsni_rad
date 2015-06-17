4cf4edf74dca15bab8fe042336b84ac801961496
Sara <sara.puljic92@gmail.com>
2015-06-17 10:36:51 +0200

    commit

--- application/models/_vti_cnf/Login.php
+++ application/models/_vti_cnf/Login.php
@@ -1,11 +1,11 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|Sara-PC\\Sara
 vti_modifiedby:SR|Sara-PC\\Sara
-vti_timelastmodified:TR|17 Jun 2015 08:11:21 -0000
+vti_timelastmodified:TR|17 Jun 2015 08:31:53 -0000
 vti_timecreated:TR|09 Jun 2015 11:47:51 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|17 Jun 2015 08:10:01 -0000
+vti_nexttolasttimemodified:TW|17 Jun 2015 08:11:21 -0000
 vti_cacheddtm:TX|17 Jun 2015 08:11:21 -0000
 vti_filesize:IR|93
 vti_cachedneedsrewrite:BR|false
